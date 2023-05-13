fc5211298c5a36f01019e56cafa2e7f04828420a
dragonnomada <dragonnomada123@gmail.com>
2023-05-13 13:54:52 -0500

    Initial Commit

---
+++ BioGraphAI/BioGraphAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioGraphAIApp.swift
+//  BioGraphAI
+//
+//  Created by Alan Badillo Salas on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BioGraphAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
